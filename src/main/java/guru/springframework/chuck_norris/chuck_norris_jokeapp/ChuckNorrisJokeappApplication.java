cf0a9731aadb8b0ebdd1fb5fd2f9b24df159bc98
TAIWO-ADEDEJI <tadedeji@systemspecscomng.onmicrosoft.com>
2024-07-22 12:25:59 +0100

    project created. Closes #1

---
+++ src/main/java/guru/springframework/chuck_norris/chuck_norris_jokeapp/ChuckNorrisJokeappApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.chuck_norris.chuck_norris_jokeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuckNorrisJokeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChuckNorrisJokeappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/chuck_norris/chuck_norris_jokeapp/ChuckNorrisJokeappApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.chuck_norris.chuck_norris_jokeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuckNorrisJokeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
